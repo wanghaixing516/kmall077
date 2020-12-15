6183035b1e1a15339e75fa9c1db779f4ac6cff43
wanghaixing_glb <2983664989@qq.com>
2020-12-15 13:08:40 +0800

    create project

---
+++ kamll-user/src/main/java/com/kgc/kmall077/kamlluser/KamllUserApplication.java
@@ -0,0 +1,13 @@
+package com.kgc.kmall077.kamlluser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KamllUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KamllUserApplication.class, args);
+    }
+
+}
---
+++ kamll-user/src/test/java/com/kgc/kmall077/kamlluser/KamllUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kgc.kmall077.kamlluser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KamllUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
