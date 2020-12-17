e5910f4c409def7e9d42a9cd8cacf301d7cd81d9
wanghaixing_glb <2983664989@qq.com>
2020-12-17 15:25:36 +0800

    十三、修改平台属性

--- kmall-manager-service/src/main/java/com/kgc/kmall/manager/mapper/PmsBaseAttrValueMapper.java
+++ kmall-manager-service/src/main/java/com/kgc/kmall/manager/mapper/PmsBaseAttrValueMapper.java
@@ -28,4 +28,6 @@ public interface PmsBaseAttrValueMapper {
     int updateByPrimaryKeySelective(PmsBaseAttrValue record);
 
     int updateByPrimaryKey(PmsBaseAttrValue record);
+    //批量添加
+    int insertBatch(@Param("attrId") Long attrId,@Param("valueList") List<PmsBaseAttrValue> valueList);
 }
--- kmall-manager-web/src/main/java/com/kgc/kmall/manager/controller/AttrController.java
+++ kmall-manager-web/src/main/java/com/kgc/kmall/manager/controller/AttrController.java
@@ -1,9 +1,11 @@
 package com.kgc.kmall.manager.controller;
 
 import com.kgc.kmall.bean.PmsBaseAttrInfo;
+import com.kgc.kmall.bean.PmsBaseAttrValue;
 import com.kgc.kmall.service.AttrService;
 import org.apache.dubbo.config.annotation.Reference;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,4 +22,16 @@ public class AttrController {
         List<PmsBaseAttrInfo> infoList = attrService.select(catalog3Id);
         return infoList;
     }
+    @RequestMapping("/saveAttrInfo")
+    public Integer saveAttrInfo(@RequestBody PmsBaseAttrInfo attrInfo){
+        Integer i = attrService.add(attrInfo);
+        return i;
+    }
+
+    @RequestMapping("/getAttrValueList")
+    public List<PmsBaseAttrValue> getAttrValueList(Long attrId){
+        List<PmsBaseAttrValue> valueList = attrService.getAttrValueList(attrId);
+        return valueList;
+    }
+
 }
